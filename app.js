2fb15681b96cdd156ca8f647b61ffea8ffc1bd3b
Wing leung CHOI <wingleungchoi@gmail.com>
2017-10-21 13:09:00 +0800

    feat/router indedx endpoint

--- app.js
+++ app.js
@@ -8,7 +8,19 @@ import { renderJsonResponse } from './lib/utils/res';
 
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url);
-  if (/^\/todo/.test(parsedUrl.path)) {
+  if (/^\/todos/.test(parsedUrl.path)) {
+    if (req.method === 'GET') {
+      // TodoController.get return promise;
+      TodoController.index(req, res);
+    } else {
+      renderJsonResponse(res, {
+        success: false,
+        errors: [
+          { message: 'Invalid Request!' },
+        ],
+      }, 400);
+    }
+  } else if (/^\/todo/.test(parsedUrl.path)) {
     if (req.method === 'GET') {
       // TodoController.get return promise;
       TodoController.get(req, res);
